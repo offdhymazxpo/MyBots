2b689f87d05e17474914a882b6857f4d2eea31d5
offdhymazxpo <76859255+offdhymazxpo@users.noreply.github.com>
2021-01-02 12:38:16 +0800

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 * JANGAN MODAL NAMA DOANG BOSQ
 * HARGAILAH YG MEMBUAT SCRIPT INI BOSQ
 * JANGAN UBAH-UBAH INFO!!!
-* FADHIL ID
+* DHYMAZ ID
 * BOLEH UBAH TAPI KECUALI INFO!!!
 */
 const qrcode = require("qrcode-terminal")
@@ -18,12 +18,12 @@ const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Jul
 const bulan = arrayBulan[moment().format('MM') - 1]
 
 const config = {
-    A187: 'NAMA BIT KALIAN🤖',
-    instagram: 'https://instagram.com/sensi.edtz_',
-    nomer: 'wa.me/+6285964224017',
-    aktif: 'TERSERAH KALIAN MAU JAM BRP AKTIFNYA',
-    youtube: 'https://youtube.sensi chanel.com',
-    whatsapp: 'terserah kalian taruh group yg mana',
+    A187: 'MY BOTS',
+    instagram: 'https://instagram.com/offdhymaz,
+    nomer: 'wa.me/+6285796582392',
+    aktif: 'Di Jam Tertentu,
+    youtube: 'https://youtube.com',
+    whatsapp: 'Onli public,
     tanggal: `TANGGAL: ${moment().format('DD')} ${bulan} ${moment().format('YYYY')}`,
     waktu: time
 }
